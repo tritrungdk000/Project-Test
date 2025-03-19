5f2a08a051dcf6c486d715f69779e8410865e59c
tritrungdk000 <dotritrungdk008@gmail.com>
2025-03-19 13:25:29 +0700

    final

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.addEventListener("scroll", function () {
 window.addEventListener("scroll", function () {
     let img = document.getElementById("scrollImg3");
     let scrollValue = window.scrollY; 
-    let moveY = Math.min(scrollValue * 0.5, 500);
+    let moveY = Math.min(scrollValue * 0.5, 200);
     img.style.transform = `translateY(-${moveY}px)`;
 });
 window.addEventListener("scroll", function () {
@@ -30,11 +30,11 @@ window.addEventListener("scroll", function () {
     let moveY = Math.min(scrollValue * 0.2, 500);
     img.style.transform = `translateY(-${moveY}px)`;
 });
-window.addEventListener("scroll", function () {
-    let img = document.getElementById("scrollImg5");
-    let scrollValue = window.scrollY;
-    let moveX = Math.min(scrollValue * 0.5, 100);
-    img.style.transform = `translateX(${moveX}px, 0px, 0px)`;
+window.addEventListener('scroll', function() {
+    let scrollImg = document.getElementById('scrollImg5');
+    let scrollPosition = window.scrollY;
+    let moveRight = Math.min(scrollPosition / 2, 200);
+    scrollImg.style.right = (105 - moveRight) + "%";
 });
 window.addEventListener("scroll", function () {
     let img = document.getElementById("bounceImg");
@@ -51,7 +51,7 @@ function countUp(elementId, start, end, duration) {
         let progress = Math.min((currentTime - startTime) / duration, 1);
         let currentNumber = Math.floor(progress * (end - start) + start);
 
-        element.innerText = currentNumber + "+"; // Thêm dấu +
+        element.innerText = currentNumber + "+";
         
         if (progress < 1) {
             requestAnimationFrame(updateNumber);
@@ -64,4 +64,21 @@ function countUp(elementId, start, end, duration) {
 document.addEventListener("DOMContentLoaded", function () {
     countUp("count", 0, 6000, 3000);
     countUp("count1", 0, 400, 3000);  
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        "https://d1j8r0kxyu9tj8.cloudfront.net/files/dl0JEyizduXZ8ZOp9tr2A7JmXswCX5Y1J8TFcCLf.png",
+        "https://d1j8r0kxyu9tj8.cloudfront.net/files/0WXBA7jbYOzN22xKKZYQzxytTNKixUIQr7BBiBd0.png"
+    ];
+    let currentImageIndex = 0;
+    const imageElement = document.querySelector(".image-section-6 img");
+
+    setInterval(() => {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        imageElement.style.opacity = 0;
+        setTimeout(() => {
+            imageElement.src = images[currentImageIndex];
+            imageElement.style.opacity = 1;
+        }, 2000);
+    }, 4000);
 });
